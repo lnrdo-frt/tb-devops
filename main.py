df3297d0daa0ed387385bb428f8cbf3ee9d6001f
lnrdo-frt <lnrdo.frt@gmail.com>
2025-03-29 18:29:54 -0300

    Update da mensagem de saída

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.python.org"  # Você pode mudar para qualquer site
+url = "https://www.poder360.com.br/"  # Você pode mudar para qualquer site
 response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
     text = soup.get_text()
     words = text.split()
-    print(f"A página {url} contém aproximadamente {len(words)} palavras.")
+    print(f"O site {url} possui cerca de {len(words)} palavras.")
 else:
     print("Erro ao acessar a página.")
